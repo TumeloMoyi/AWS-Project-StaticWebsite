b705018bfd85c49b5007e6da042a81f2e102e785
NeonCode <moyitumelo65@gmail.com>
2026-05-23 18:19:02 +0200

    AWS Project 1.2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-console.log("Baía Restaurant Website Loaded");
+console.log("Baía Website Loaded");
+
+
+const cards =
+  document.querySelectorAll(".card");
+
+
+window.addEventListener("scroll", () => {
+
+  cards.forEach(card => {
+
+    const cardTop =
+      card.getBoundingClientRect().top;
+
+    if(cardTop < window.innerHeight - 100){
+
+      card.style.opacity = "1";
+
+      card.style.transform =
+        "translateY(0)";
+
+    }
+
+  });
+
+});
+
+
+cards.forEach(card => {
+
+  card.style.opacity = "0";
+
+  card.style.transform =
+    "translateY(50px)";
+
+  card.style.transition =
+    "all 1s ease";
+
+});
