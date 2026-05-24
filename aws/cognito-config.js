37f792d105405697ccfd72ee251a8c44f9d28fe8
Promise Sibanda <brilliantpromise0@gmail.com>
2026-05-24 15:27:55 +0200

    Promise added Cognito configuration file

--- aws/cognito-config.js
+++ aws/cognito-config.js
@@ -1,9 +1,7 @@
-const poolData = {
-
-  UserPoolId: "POOL_ID",
-
-  ClientId: "CLIENT_ID"
-
+export const cognitoConfig = {
+    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_QBvnBnoeu",
+    client_id: "uiaftai0t6g5jiorv6qpslrtb",
+    redirect_uri: "http://127.0.0.1:5500/",
+    response_type: "code",
+    scope: "email openid phone"
 };
-
-const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
